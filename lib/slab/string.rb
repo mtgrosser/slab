b6254e543d9cd314cd5c38c8af0096bb3f861d2f
Matthias Grosser <mtgrosser@gmx.net>
2012-07-04 23:59:17 +0300

    Fix string generation, add set parameter

--- lib/slab/string.rb
+++ lib/slab/string.rb
@@ -5,7 +5,9 @@ class String
     encode('iso-8859-1', :invalid => :replace, :undef => :replace)
   end
 
-  def self.random(length = 12)
-    SecureRandom.base64(length)[0..(length - 1)]
+  def self.random(length = 12, set = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+    set_size, string = set.size, ''
+    length.times { string << set[SecureRandom.random_number(set_size)] }
+    string
   end
 end
